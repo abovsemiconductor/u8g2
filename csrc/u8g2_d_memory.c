2dd499db107efa22cadab882e963e811b3009672
sangwoon.lee <sangwoon.lee@abov.co.kr>
2026-06-16 20:24:00 +0900

    [PLATFORM][LIBRARY]
    1. Author ID
       Hyunseok.choi
    
    2. Issue
       INT
    
    3. Change
       Added EX_SSD1309 temporarily.
       - There is firmware size issue to use U8g2.

--- csrc/u8g2_d_memory.c
+++ csrc/u8g2_d_memory.c
@@ -3,6 +3,19 @@
 
 #include "u8g2.h"
 
+#if EX_SSD1309 
+uint8_t *u8g2_m_16_8_1(uint8_t *page_cnt)
+{
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 1;
+  return 0;
+  #else
+  static uint8_t buf[128];
+  *page_cnt = 1;
+  return buf;
+  #endif
+}
+#else
 uint8_t *u8g2_m_16_4_1(uint8_t *page_cnt)
 {
   #ifdef U8G2_USE_DYNAMIC_ALLOC
@@ -2313,4 +2326,5 @@ uint8_t *u8g2_m_48_30_f(uint8_t *page_cnt)
   return buf;
   #endif
 }
+#endif
 /* end of generated code */
