c4d0a6a14515ef2122697bc18a4bc6ac2517a04b
sangwoon.lee <sangwoon.lee@abov.co.kr>
2026-06-16 20:37:59 +0900

    [APPLICATION][SOURCE][EVB][AIRCLOCK]
    1. Author ID
       Jaehyuk.Jang
    
    2. Issue
       INT
    
    3. Change
       change to use full buffer function.
       change to build font u8g2_font_6x10_tr, u8g2_font_10x20_tf.

--- csrc/u8g2_d_memory.c
+++ csrc/u8g2_d_memory.c
@@ -15,6 +15,17 @@ uint8_t *u8g2_m_16_8_1(uint8_t *page_cnt)
   return buf;
   #endif
 }
+uint8_t *u8g2_m_16_8_f(uint8_t *page_cnt)
+{
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 8;
+  return 0;
+  #else
+  static uint8_t buf[1024];
+  *page_cnt = 8;
+  return buf;
+  #endif
+}
 #else
 uint8_t *u8g2_m_16_4_1(uint8_t *page_cnt)
 {
